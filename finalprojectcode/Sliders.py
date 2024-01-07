d0f5cd4e237c7c3d3ffd157d46018af5dad9a839
Ruth Berkun <67928066+happyRainDrop@users.noreply.github.com>
2024-01-06 22:47:31 -0800

    update slider spider coords to work with rendering

--- finalprojectcode/Sliders.py
+++ finalprojectcode/Sliders.py
@@ -51,133 +51,7 @@ class Sliders():
 
         return J
 
-    ### Why is this not working??? Entries completely mismatch compute_jacobian
-    ### Not being used currently.
-    def invJac(self):
-        '''Given Given q = [Tx, Ty, Tz, psi, theta, phi],
-        Jaq(q) returns J(q) s.t. J(q)*q'= dx/dt'''
-        q=self.q
-        Tx = q[0]
-        Ty = q[1]
-        Tz = q[2]
-        psi = q[3]
-        theta = q[4]
-        phi = q[5]
-
-        X = self.invkin(q)
-        J = [] # Fill below
-
-
-        J_expected = []
-        psi_0 = q[3]
-        theta_0 = q[4]
-        phi_0 = q[5]
-
-        for i in range(6):
-            L = X[i]
-            p = np.array([self.top_pos[i][0]-self.center_pos[0], 
-                        self.top_pos[i][1]-self.center_pos[1],
-                        self.top_pos[i][2]-self.center_pos[2]]).transpose()  # Vector between point on top and center of top
-
-            ######################################## HAND-CALCULATED DERIVATIVE ####################################
-            dlx_dphi = (np.sin(psi)*np.sin(phi)+np.cos(psi)*np.sin(theta)*np.cos(phi))*p[1] 
-            + (np.sin(psi)*np.cos(phi)-np.cos(psi)*np.sin(theta)*np.sin(phi))*p[2]
-            
-            dlx_dtheta = (-np.cos(psi)*np.sin(theta))*p[0]
-            + (np.cos(psi)*np.cos(theta)*np.sin(phi))*p[1]
-            + (np.cos(psi)*np.cos(theta)*np.cos(phi))*p[2]
-
-            dlx_dpsi = (-np.sin(psi)*np.cos(theta))*p[0]
-            + (-np.cos(psi)*np.cos(phi)-np.sin(psi)*np.sin(theta)*np.sin(phi))*p[1]
-            + (np.cos(psi)*np.sin(phi)-np.sin(psi)*np.sin(theta)*np.cos(phi))*p[2]
-
-            dly_dphi = (-np.cos(psi)*np.sin(phi)+np.sin(psi)*np.sin(theta)*np.cos(phi))*p[1]
-            + (-np.cos(psi)*np.cos(phi)-np.sin(psi)*np.sin(theta)*np.sin(psi))*p[2]
-
-            dly_dtheta = (-np.sin(psi)*np.sin(theta))*p[0]
-            +(np.sin(psi)*np.cos(theta)*np.sin(phi))*p[1]
-            +(np.sin(psi)*np.cos(theta)*np.cos(phi))*p[2]
-
-            dly_dpsi = (np.cos(psi)*np.cos(theta))*p[0]
-            +(-np.sin(psi)*np.cos(phi)+np.cos(psi)*np.sin(theta)*np.sin(psi))*p[1]
-            +(np.sin(psi)*np.sin(phi)+np.cos(psi)*np.sin(theta)*np.cos(phi))*p[2]
-
-            dlz_dphi = (np.cos(theta)*np.cos(phi))*p[1]
-            +(-np.cos(theta)*np.sin(phi))*p[2]
-
-            dlz_dtheta = (-np.cos(theta))*p[0]
-            +(-np.sin(theta)*np.sin(phi))*p[1]
-            +(-np.sin(theta)*np.cos(phi))*p[2]
-
-            dlz_dpsi = 0
-
-             # Total partial derivatives
-            dl_dphi = dlx_dphi + dly_dphi + dlz_dphi
-            dl_dtheta = dlx_dtheta + dly_dtheta + dlz_dtheta
-            dl_dpsi = dlx_dpsi + dly_dpsi + dlz_dpsi
-            J.append(np.array([1/L, 1/L, 1/L, dl_dpsi/L, dl_dtheta/L, dl_dphi/L]))
-
-            
-            ######################################## COMPUTER-CALCULATED DERIVATIVE ####################################
-            psi_1, theta_1, phi_1 = symbols('psi_1 theta_1 phi_1')
-            cos_psi = cos(psi_1)
-            sin_psi = sin(psi_1)
-            cos_theta = cos(theta_1)
-            sin_theta = sin(theta_1)
-            cos_phi = cos(phi_1)
-            sin_phi = sin(phi_1)
-            
-            R_matrix = [p[0]*(cos_psi*cos_theta) + p[1]*(-sin_psi*cos_phi + cos_psi*sin_theta*sin_phi) + p[2]*(sin_psi*sin_phi + cos_psi*sin_theta*cos_phi), 
-                    p[0]*(sin_psi*cos_theta) + p[1]*(cos_psi*cos_phi + sin_psi*sin_theta*sin_phi) + p[2]*(-cos_psi*sin_phi + sin_psi*sin_theta*cos_phi), 
-                    p[0]*-sin_theta + p[1]*(cos_theta*sin_phi) + p[2]*(cos_theta*cos_phi)]
-            l_i = R_matrix
-
-            subs_dict = {phi_1: phi_0, theta_1: theta_0, psi_1: psi_0}
-
-            dlx_dphi_1 = diff(l_i[0], phi_1).subs(subs_dict)
-            
-            dlx_dtheta_1 = diff(l_i[0], theta_1).subs(subs_dict)
-
-            dlx_dpsi_1 = diff(l_i[0], psi_1).subs(subs_dict)
-
-            dly_dphi_1 = diff(l_i[1], phi_1).subs(subs_dict)
-
-            dly_dtheta_1 = diff(l_i[1], theta_1).subs(subs_dict)
-
-            dly_dpsi_1 = diff(l_i[1], psi_1).subs(subs_dict)
-
-            dlz_dphi_1 = diff(l_i[2], phi_1).subs(subs_dict)
-            
-            dlz_dtheta_1 = diff(l_i[2], theta_1).subs(subs_dict)
-
-            dlz_dpsi_1 = diff(l_i[2], psi_1).subs(subs_dict)
-
-            # Total partial derivatives
-            dl_dphi_1 = dlx_dphi_1 + dly_dphi_1 + dlz_dphi_1
-            dl_dtheta_1 = dlx_dtheta_1 + dly_dtheta_1 + dlz_dtheta_1
-            dl_dpsi_1 = dlx_dpsi_1 + dly_dpsi_1 + dlz_dpsi_1
-            J_expected.append(np.array([1/L, 1/L, 1/L, float(dl_dpsi_1/L), float(dl_dtheta_1/L), float(dl_dphi_1/L)]))
-            
-        
-        '''Debugging of derivatives of fkin matrix
-        print("JACOBIAN =======================")
-        s = [[str(e) for e in row] for row in J]
-        lens = [max(map(len, col)) for col in zip(*s)]
-        fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-        table = [fmt.format(*row) for row in s]
-        print('\n'.join(table))
-
-
-        print("OTHER JACOBIAN =======================")
-        s = [[str(e) for e in row] for row in J_expected]
-        lens = [max(map(len, col)) for col in zip(*s)]
-        fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-        table = [fmt.format(*row) for row in s]
-        print('\n'.join(table))
-        '''
-        
-
-        return J_expected
+ 
 
     def fkin(self, xgoal, qstart = []):
         '''
@@ -267,7 +141,7 @@ class Sliders():
 
             spider_q.append(pitch)
             spider_q.append(roll)
-            spider_q.append(leg_length)
+            spider_q.append(leg_length - 2.4)
 
         return spider_q
 
